9aeaac3469353df56f91acc212dc7e4218a00069
Neil Hughes <enhughesiasm@googlemail.com>
2023-03-04 15:40:38 +0000

    feat: Coding Tile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
 			fontFamily: {
 				sans: ['Jost', 'sans-serif'],
 				serif: ['Besley', 'Georgia', 'serif'],
+				code: ['VT323', 'Courier New', 'Courier', 'monospace'],
 			},
 			animation: {
 				'ping-once': 'ping 1s ease-in-out 1',
@@ -13,8 +14,15 @@ module.exports = {
 				'text-bounce-once': 'text-bounce .5s ease-in-out 1',
 				'heartbeat-fast': 'heartbeat 1s infinite',
 				'heartbeat-slow': 'heartbeat 3s infinite',
+				'cursor-blink':
+					'blink 0.7s cubic-bezier(0.5, 0, 1, 1) infinite alternate ',
 			},
 			keyframes: {
+				blink: {
+					'0%': {
+						opacity: 0,
+					},
+				},
 				'bounce-gentle': {
 					'0%': { transform: 'scale(1,1) translateY(0)' },
 					'10%': { transform: 'scale(1.05,.95) translateY(0)' },
