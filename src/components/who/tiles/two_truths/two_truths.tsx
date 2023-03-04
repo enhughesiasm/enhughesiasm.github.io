83decbf3d2e849090b176dd7cce88d78f3421307
Neil Hughes <enhughesiasm@googlemail.com>
2023-03-04 16:24:54 +0000

    feat: Home tiles

---
+++ src/components/who/tiles/two_truths/two_truths.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { truths } from '../../../../data/truths/truths';
+import lies from '../../../../data/truths/lies.json';
+
+const choose = <T,>(arr: T[], number: number) => {
+	const ret: T[] = [];
+
+	while (number > 0) {
+		const candidate = arr[Math.floor(Math.random() * arr.length)];
+		if (!ret.includes(candidate)) {
+			ret.push(candidate);
+			number--;
+		}
+	}
+
+	return ret;
+};
+
+const getSelectedTruths = () => {
+	const selectedTruths = choose(truths, 2);
+	const selectedLie = choose(lies, 1);
+
+	return selectedTruths
+		.map((t) => {
+			return { truth: true, text: t, clicked: false };
+		})
+		.concat(
+			selectedLie.map((l) => {
+				return { truth: false, text: l, clicked: false };
+			})
+		)
+		.sort(() => Math.random() - 0.5);
+};
+
+export const TwoTruths = () => {
+	const [selected, setSelected] = useState(getSelectedTruths());
+
+	return (
+		<>
+			<ul className='mt-3 space-y-3'>
+				{selected.map((item) => (
+					<li
+						className={`cursor-pointer text-lg font-bold ${
+							item.clicked && !item.truth
+								? 'text-red-500 line-through'
+								: ''
+						}
+						${item.clicked && item.truth ? 'text-green-500' : ''}
+						`}
+						onClick={() => {
+							const newSelected = [...selected];
+							const newItem = newSelected.find(
+								(a) => a.text === item.text
+							);
+							if (newItem) {
+								newItem.clicked = true;
+							}
+							setSelected(newSelected);
+						}}>
+						{item.text}
+					</li>
+				))}
+			</ul>
+			<hr />
+			<div className='mt-6'>
+				<a
+					className='rounded-xl bg-green-500 py-2 px-6 text-white hover:bg-green-400'
+					onClick={() => setSelected(getSelectedTruths())}>
+					get more
+				</a>
+			</div>
+		</>
+	);
+};
---
+++ src/data/truths/truths.ts
@@ -0,0 +1,21 @@
+import { TED_TALK_VIEWS } from '../definitions';
+
+export const truths = [
+	'... technically invented a method of distinguishing radioactive isotopes',
+	'... once experienced hallucinations due to altitude sickness',
+	'... plays the tuba',
+	'... has lived on three continents',
+	`... gave a TED talk which has been viewed over ${TED_TALK_VIEWS.toLocaleString()} times`,
+	'... can (sorta) speak five languages',
+	'... once ate three meals at one sitting due to excessive politeness',
+	'... accidentally assaulted somebody with a banana while performing at the Edinburgh Fringe in 2003',
+	'... started computer programming as a child, on a ZX Spectrum +2 in the 1980s',
+	"... 's first big public speech was about his fear of public speaking",
+	'... knows all the words to Whenever, Wherever by Shakira in the original Spanish',
+	'... drove 4000km in two days to attend the Champions League Final in 2019',
+	"... doesn't have any tonsils",
+	'... once lived for months in an abandoned swimming pool changing room in a desert',
+	'... was once (wrongly) known as Rowan to an entire town in rural Australia',
+	'... has broken one bone and chipped another',
+	'... is allergic to apples',
+];
